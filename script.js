ea069c03bb586176e20f936fb9baa497f67d4546
Панков Артем Владимирович <pankov_AV@GOIK.LOCAL>
2022-03-22 17:05:58 +0300

    Added changes

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
 let myage = document.getElementById('age');
 myage.innerText = getAge();
 
-// function getAge() {
-//     let birthday = new Date(1994, 4, 6),
-//         today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()),
-//         age = today.getFullYear() - birthday.getFullYear();
-//     if (today < new Date(today.getFullYear(), birthday.getMonth(), birthday.getDate())) {
-//         age = age - 1;
-//     }
-//     return age;
-// }
-
-function getAge(){
+function getAge() {
     let birthday = new Date(1994, 4, 6),
-        today = new Date();
-    let diff = today.getTime() - birthday.getTime();
-    return  new Date(diff).getFullYear();
+        today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()),
+        age = today.getFullYear() - birthday.getFullYear();
+    if (today < new Date(today.getFullYear(), birthday.getMonth(), birthday.getDate())) {
+        age = age - 1;
+    }
+    return age;
 }
 
 let layoutChangeBtn = document.getElementById('layout-change');
